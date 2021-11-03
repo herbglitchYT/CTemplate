f2fe0b1781bc970f052bddcd46f99d2083ffb6f1
herbglitch <bighebe@gmail.com>
2021-11-03 15:34:36 -0600

    conf file udpate, added a simple command system and command folder, errors now have a system, will now check to make sure all args passed are ok

---
+++ src/commands/commands.h
@@ -0,0 +1,14 @@
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+int ctemp_help();
+
+/*
+usage: git [--version] [--help] [-C <path>] [-c <name>=<value>]
+           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
+           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
+           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
+           [--super-prefix=<path>] [--config-env=<name>=<envvar>]
+           <command> [<args>]
+*/
+#endif
